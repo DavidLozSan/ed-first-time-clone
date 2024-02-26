d7d92cb2ba657164cc370fbc247754e0fdf8fca9
DavidLozSan <davidloz94.dls@gmail.com>
2024-02-26 18:04:16 +0100

    intento de test unitario

--- src/test/java/com/iesam/firsttime/features/vote/domain/VoteAppUseCaseTest.java
+++ src/test/java/com/iesam/firsttime/features/vote/domain/VoteAppUseCaseTest.java
@@ -1,17 +1,38 @@
 package com.iesam.firsttime.features.vote.domain;
 
+import com.iesam.firsttime.features.appconfig.domain.AppConfigRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VoteAppUseCaseTest {
 
+    private VoteAppUseCase voteAppUseCase;
+
     @BeforeEach
     void setUp() {
+        //Cuando se termine de ejecutar el test, pongo a nulo el caso de uso
+
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void cuandoAppConfigEsNuloEntoncesNoPuedoVotar(){
+        //Given
+        voteAppUseCase = new VoteAppUseCase();
+
+
+        //When
+        Boolean result = voteAppUseCase.execute();
+
+
+        //Then: Asserts
+        Assertions.assertTrue();
+
     }
 }
